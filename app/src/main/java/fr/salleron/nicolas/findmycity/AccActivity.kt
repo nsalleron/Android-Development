53a7e58985e4376a3863b3d23e6d2e21c43a2a02
Nicolas SALLERON <nicolas.salleron@gmail.com>
2018-01-08 16:46:59 +0100

    first commit

---
+++ app/src/main/java/fr/salleron/nicolas/findmycity/AccActivity.kt
@@ -0,0 +1,12 @@
+package fr.salleron.nicolas.findmycity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AccActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acc)
+    }
+}
